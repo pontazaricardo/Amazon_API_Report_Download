f8f3f36fa59a914afd50d451652cf1232ae872c0
pontazaricardo <pontaza.ricardo@gmail.com>
2017-12-20 18:01:21 +0800

    Update

--- main/Amazon_API_Report_Download/Amazon_API_Report_Download/Program.cs
+++ main/Amazon_API_Report_Download/Amazon_API_Report_Download/Program.cs
@@ -5,6 +5,7 @@ using System.Text;
 using System.Threading.Tasks;
 
 using MarketplaceWebService;
+using MarketplaceWebService.Model;
 
 namespace Amazon_API_Report_Download
 {
@@ -15,7 +16,7 @@ namespace Amazon_API_Report_Download
 
         }
 
-        private List<Tuple<string,DateTime,DateTime>> getReportsIds(MarketplaceWebService.MarketplaceWebService service, MarketplaceWebService.Model.GetReportListRequest request)
+        private List<Tuple<string,DateTime,DateTime>> getReportsIds(MarketplaceWebService.MarketplaceWebService service, GetReportListRequest request)
         {
             List<Tuple<string, DateTime, DateTime>> reports = new List<Tuple<string, DateTime, DateTime>>(); //Includes the reportId, dateFrom and dateTo for a settlement report.
 
@@ -24,6 +25,29 @@ namespace Amazon_API_Report_Download
                 return reports;
             }
 
+            try
+            {
+                List<string> reportIds = new List<string>();
+                List<DateTime> availableDates = new List<DateTime>();
+
+                GetReportListResponse response = service.GetReportList(request);
+
+                if (response.IsSetGetReportListResult())
+                {
+                    GetReportListResult getReportListResult = response.GetReportListResult;
+                    List<ReportInfo> reportInfoList = getReportListResult.ReportInfo;
+                    foreach (ReportInfo reportInfo in reportInfoList)
+                    {
+                        reportIds.Add(reportInfo.ReportId);
+                        availableDates.Add(reportInfo.AvailableDate);
+                    }
+                }
+
+            }
+            catch(Exception e)
+            {
+
+            }
         }
     }
 }
